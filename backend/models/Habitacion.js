c1374b36c4099ae63091a9a114010503e0d07a29
K1pler <roland@roland.com>
2025-02-04 23:52:28 +0100

    Primer commit: configuración inicial del proyecto

--- backend/models/Habitacion.js
+++ backend/models/Habitacion.js
@@ -3,7 +3,7 @@ const pool = require('../db'); // Conexión a PostgreSQL
 // Obtener todas las habitaciones (sin JOIN, solo la tabla habitaciones)
 const obtenerHabitaciones = async () => {
   const result = await pool.query(`
-    SELECT id, nombre, descripcion, metros_cuadrados, tipo_id 
+    SELECT id, nombre, descripcion, metros_cuadrados, id 
     FROM habitaciones
   `);
   return result.rows;
